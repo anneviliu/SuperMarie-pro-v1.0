eccc1110aaa86eb9ae4e219df456efa977cd0abf
文浩 柳 <1150925502@qq.com>
2018-11-26 14:13:13 +0800

    DISPLAY 更改

--- SuperMarie pro-v1.0/MarieDisplayFunction.cpp
+++ SuperMarie pro-v1.0/MarieDisplayFunction.cpp
@@ -7,22 +7,19 @@
 
 void menu_show()
 {
-	int i=0;
-	char play[] = _T("Play Now!");
+	int i = 0;
 	IMAGE img_preplay;
 	initgraph(WIDTH, HIGH);
 	loadimage(&img_preplay, _T("res\\level_1.png"));
 	mciSendString("open res\\背景音乐.mp3 alias music_back", NULL, 0, NULL);
 	mciSendString("play music_back", NULL, 0, NULL);
-	
-	
-	putimage(0, 0, WIDTH, HIGH, &img_preplay, 0, 0);
+	putimage(0, 0, WIDTH, HIGH, &img_preplay, i, 0);
 	setbkmode(TRANSPARENT);
 	settextcolor(BLACK);
 	settextstyle(50, 0, _T("黑体"));
 	outtextxy(WIDTH*0.4, HIGH*0.4, "进入游戏");
-	outtextxy(WIDTH*0.4, HIGH*0.4, "设置");
-		
+	outtextxy(WIDTH*0.425, HIGH*0.5, "设置");
+
 	FlushBatchDraw();
 	Sleep(2);
 
