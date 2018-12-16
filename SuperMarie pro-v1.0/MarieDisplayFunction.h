028b2ebcea2fe3906aa195a52f6af5009384ff29
dobie <1093557432@qq.com>
2018-12-16 11:05:02 +0800

    啥都有  但是有bug  还不能跳

--- SuperMarie pro-v1.0/MarieDisplayFunction.h
+++ SuperMarie pro-v1.0/MarieDisplayFunction.h
@@ -5,7 +5,7 @@
 
 
 void GameStart(); //游戏开始主界面
-void show();
+void hero_show();
 void game_show();
 void HelpPage();  //游戏操作说明界面
 void Introduction(); //游戏简介界面
@@ -14,5 +14,6 @@ void Introduction(); //游戏简介界面
 void begin();
 void preload();
 void map_show();
+
 
 #endif
